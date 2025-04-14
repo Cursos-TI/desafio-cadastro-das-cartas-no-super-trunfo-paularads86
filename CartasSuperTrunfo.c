c8ad02f882975f05bbc490d945e130ca0bfffb4e
paularads86 <paularads86@gmail.com>
2025-04-14 14:03:38 +0000

    Realizei o desafio Novato em 13-04-2025

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -18,5 +18,63 @@ int main() {
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
 
+    
+    char estado1[20], estado2[20];
+    char codigo1[20], codigo2[20];
+    char cidade1[20], cidade2[20];
+    int populacao1, populacao2;
+    int pontosTuristicos1, pontosTuristicos2;
+    float area1, area2;
+    float PIB1, PIB2;
+
+    printf("Carta1\n");
+    
+    printf("Digite o seu estado:");
+    scanf("%s", estado1);
+
+    printf("Digite o código da carta:");
+    scanf("%s", &codigo1);
+
+    printf("Digite a sua cidade:");
+    scanf("%s", cidade1);
+
+    printf("Digite a população:");
+    scanf("%d", &populacao1);
+
+    printf("Digite os pontos turísticos:");
+    scanf("%d", &pontosTuristicos1);
+
+    printf("Digite sua área:");
+    scanf("%f", &area1);
+
+    printf("Digite seu PIB:");
+    scanf("%f", &PIB1);
+
+
+    printf("Carta2\n");
+    
+    printf("Digite o seu estado:");
+    scanf("%s", estado2);
+
+    printf("Digite o código da carta:");
+    scanf("%s", &codigo2);
+
+    printf("Digite a sua cidade:");
+    scanf("%s", cidade2);
+
+    printf("Digite a população:");
+    scanf("%d", &populacao2);
+
+    printf("Digite os pontos turísticos:");
+    scanf("%d", &pontosTuristicos2);
+
+    printf("Digite sua área:");
+    scanf("%f", &area2);
+
+    printf("Digite seu PIB:");
+    scanf("%f", &PIB2);
+    
+    
+
     return 0;
 }
